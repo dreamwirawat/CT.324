0fa5f50d56deea49b60b4b9887070b661eeeb430
dreamwirawat <120708424+dreamwirawat@users.noreply.github.com>
2023-06-30 14:12:13 +0700

    Add files via upload

---
+++ work1.ino
@@ -0,0 +1,23 @@
+#define ldr A2
+#define led 13
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ldr, INPUT);
+  pinMode(led, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int ldr_ana = analogRead(ldr);
+  Serial.println(ldr_ana);
+  if(ldr_ana > 500){
+    digitalWrite(led,HIGH);
+  }else {
+    digitalWrite(led,LOW);
+  }
+  
+  
+}
