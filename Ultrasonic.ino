ccf6df7c4e8e83eb99e9ea6c8b2f95412fababbb
dreamwirawat <120708424+dreamwirawat@users.noreply.github.com>
2023-07-07 14:33:47 +0700

    Add files via upload

---
+++ Ultrasonic.ino
@@ -0,0 +1,31 @@
+#define pingPin 13
+#define inPin 12
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pingPin, OUTPUT);
+  pinMode(inPin, INPUT);
+}
+
+void loop(){
+  float duration, cm;
+  
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+  
+  duration = pulseIn(inPin, HIGH);
+  cm = (duration/ 1000000)*173 *100;
+
+  Serial.println(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  delay(500);
+}
+
+//40 hz ulatrasonic
+
